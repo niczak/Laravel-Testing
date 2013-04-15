7f5fe1501b07c20102972b170b2c7e27ae37c07b
Nicholas Kreidberg <nkreidberg@spydertrap.com>
2013-04-15 18:38:03 -0500

    Added links back into user profiles as well as URLs included in the tweet(s).

--- application/views/twitter/index.blade.php
+++ application/views/twitter/index.blade.php
@@ -6,6 +6,20 @@
 	<title>Laravel: Testing &amp; Breaking Stuff</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('laravel/css/style.css') }}
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$(".tweetbody").each(function() {
+			var tweet = replaceURLWithHTMLLinks($(this).html());
+			$(this).html(tweet);
+		});
+	});
+	
+	function replaceURLWithHTMLLinks(text) {
+	    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	    return text.replace(exp,"<a href='$1' target=\"_blank\">$1</a>"); 
+	}
+	</script>
 </head>
 <body>
 	<div class="wrapper">
@@ -20,15 +34,14 @@
 			<div class="home">
 <h2>Testing Twitter API</h2>
 <?php  
-// echo "<pre>";
-// var_dump($tweets->results);
-// echo "</pre>";
-
 foreach($tweets->results as $tweet) {
-	printf("<div class=\"tweet\"><img src=\"%s\" />%s</div>\n", 
-		$tweet->profile_image_url, $tweet->text);
+	printf("<div class=\"tweet\"><a href=\"http://twitter.com/%s\" target=\"_blank\"><img src=\"%s\" /></a><span class=\"tweetbody\">%s</span></div>\n", 
+		$tweet->from_user, $tweet->profile_image_url, $tweet->text);
 }
 
+echo "<pre>";
+var_dump($tweets->results);
+echo "</pre>";
 
 ?>
 			</div>
